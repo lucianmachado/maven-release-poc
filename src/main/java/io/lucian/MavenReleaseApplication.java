f684decc9e08320fade493afcf2d127396dfc6a3
Lucian Machado <lucian.m.oliveira@gmail.com>
2019-01-05 02:30:38 -0200

    Inicio projeto.

---
+++ src/main/java/io/lucian/MavenReleaseApplication.java
@@ -0,0 +1,14 @@
+package io.lucian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenReleaseApplication.class, args);
+    }
+
+}
+
